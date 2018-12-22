94a802965a6065369be16d586b55cbf598082607
danplo1 <budweiser401@wp.pl>
2018-12-22 20:48:26 +0100

    SpringBootappdemo_sceurityConfig

--- appdemo/src/main/java/com/dan/appdemo/AppdemoApplication.java
+++ appdemo/src/main/java/com/dan/appdemo/AppdemoApplication.java
@@ -2,8 +2,10 @@ package com.dan.appdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class AppdemoApplication {
 
 	public static void main(String[] args) {

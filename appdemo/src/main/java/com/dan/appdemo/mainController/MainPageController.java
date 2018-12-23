22d7b7ff49fa935d1b73e234de0869123054c4c3
danplo1 <budweiser401@wp.pl>
2018-12-23 20:34:44 +0100

    Beginnings (configuration)from Spring Boot #01 by AR

---
+++ appdemo/src/main/java/com/dan/appdemo/mainController/MainPageController.java
@@ -0,0 +1,17 @@
+package com.dan.appdemo.mainController;
+
+import javax.ws.rs.GET;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+
+	@GET
+	@RequestMapping("/")
+	public String showMainPAge() {
+		return "index";
+	}
+
+}

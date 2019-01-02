16f4b143120bbee3ab877e713d03d7ba5620fe63
danplo1 <budweiser401@wp.pl>
2019-01-02 22:18:47 +0100

    Spring Boot #04 by AR, User entry to the database part.1

--- appdemo/src/main/java/com/dan/appdemo/user/RegisterController.java
+++ appdemo/src/main/java/com/dan/appdemo/user/RegisterController.java
@@ -1,23 +1,62 @@
 package com.dan.appdemo.user;
 
-import javax.ws.rs.GET;
+import java.util.Locale;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dan.appdemo.validators.UserRegisterValidator;
 
 @Controller
 public class RegisterController {
 
-	
-	@GET //odebranie danych
-	@RequestMapping(value = "/register") //obsługa wywołania
-	public String registerForm (Model model) {
-		
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	MessageSource messageSource; // pobiera komunikaty z meesage.properties komunikaty w kodzie Java
+
+	@GET // odebranie danych
+	@RequestMapping(value = "/register") // obsługa wywołania
+	public String registerForm(Model model) {
+
 		User u = new User();
 		model.addAttribute("user", u);
 		return "registred";
-		
+	}
+
+	@POST
+	@RequestMapping(value = "/adduser")
+	public String registerAction(User user, BindingResult result, Model model, Locale locale) {
+
+		String returnPage = null;
+
+		User userExist = userService.findUserByEmail(user.getEmail());
+		new UserRegisterValidator().validate(user, result);
+
+		if (userExist != null) {
+			result.rejectValue("email", messageSource.getMessage("error.userEmailExist", null, locale));
+		}
+
+		if (result.hasErrors()) {
+			returnPage = "register";
+		} else {
+			
+			userService.saveUser(user);
+			model.addAttribute("message", messageSource.getMessage("user.register.success.email", null, locale));
+			model.addAttribute("user", new User());
+			returnPage = "index";
+		}
+
+		return returnPage;
+
 	}
 
 }
---
+++ appdemo/src/main/java/com/dan/appdemo/user/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.dan.appdemo.user;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+@Transactional // włącza transakcje jeżeli chodzi o zapis danych
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Override
+	public User findUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@Override
+	public void saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setActive(1); // ustawienie wartości czy użytkownik może się logować (user: 0-nieaktywny, 1-aktywny)
+
+		Role role = roleRepository.findByRole("ROLE_ADMIN");
+		user.setRoles(new HashSet<Role>(Arrays.asList(role)));
+
+		userRepository.save(user); // metoda do zapisywania naszego użytkownika w bazie danych
+
+	}
+
+}

17b38237e89423494d413a7563a86f6584b1f77f
danplo1 <budweiser401@wp.pl>
2019-01-08 22:33:14 +0100

    Spring Boot #06 - creating a controller

---
+++ appdemo/src/main/java/com/dan/appdemo/user/ProfilController.java
@@ -0,0 +1,36 @@
+package com.dan.appdemo.user;
+
+import javax.ws.rs.GET;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dan.appdemo.utilities.UserUtilities;
+
+@Controller
+public class ProfilController {
+
+	@Autowired
+	private UserService userService;
+
+	@GET
+	@RequestMapping(value = "/profil")
+	public String showUserProfilePage(Model model) {
+		String username = UserUtilities.getLoggedUser();
+
+		User user = userService.findUserByEmail(username);
+		
+		int RoleNumber = user.getRoles().iterator().next().getId();
+		
+		user.setRoleNumber(RoleNumber);
+		model.addAttribute("user", user);
+		
+		
+
+		return "profil";
+
+	}
+
+}

8d6479de6d972ed158283804c14c4f8c39b5f3da
danplo1 <budweiser401@wp.pl>
2019-01-15 21:59:38 +0100

    Spring Boot # 07 - Change password. @Query Annotation //ProfilController

--- appdemo/src/main/java/com/dan/appdemo/user/ProfilController.java
+++ appdemo/src/main/java/com/dan/appdemo/user/ProfilController.java
@@ -1,19 +1,28 @@
 package com.dan.appdemo.user;
 
+import java.util.Locale;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dan.appdemo.utilities.UserUtilities;
+import com.dan.appdemo.validators.ChangePasswordValidator;
 
 @Controller
 public class ProfilController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@GET
 	@RequestMapping(value = "/profil")
@@ -21,25 +30,45 @@ public class ProfilController {
 		String username = UserUtilities.getLoggedUser();
 
 		User user = userService.findUserByEmail(username);
-		
+
 		int RoleNumber = user.getRoles().iterator().next().getId();
-		
+
 		user.setRoleNumber(RoleNumber);
 		model.addAttribute("user", user);
-		
+
 		return "profil";
 
 	}
-	
+
 	@GET
 	@RequestMapping(value = "/editpassword")
-	public String editUserPassword (Model model) {
+	public String editUserPassword(Model model) {
 		String username = UserUtilities.getLoggedUser();
 		User user = userService.findUserByEmail(username);
 		model.addAttribute("user", user);
 		return "editpassword";
 	}
-	
-	
+
+	@POST
+	@RequestMapping(value = "/updatepassword")
+	public String changeUserPassword(User user, BindingResult result, Model model, Locale locale) { // rejestracja
+																									// uzytkowanika, 4 argumenty w metodzie
+																								
+		String returnPage = null;
+
+		new ChangePasswordValidator().validate(user, result);
+		new ChangePasswordValidator().checkPasswords(user.getNewPassword(), result);// sprawdz czy podalismy hasło,
+																					// jeżeli pole nie jest puste
+
+		if (result.hasErrors()) {
+			returnPage = "editpassword";
+		} else {
+			userService.updateUserPassword(user.getNewPassword(), user.getEmail());
+			returnPage = "editpassword";
+			model.addAttribute("message", messageSource.getMessage("passwordChange.success", null, locale));
+		}
+		return returnPage;
+
+	}
 
 }

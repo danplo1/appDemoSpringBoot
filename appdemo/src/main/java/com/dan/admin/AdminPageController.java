2928278154525161d11e4e8a3564167dc1049111
danplo1 <budweiser401@wp.pl>
2019-01-24 18:31:10 +0100

    Spring Boot # 09 - @Secured. Admin panel

--- appdemo/src/main/java/com/dan/admin/AdminPageController.java
+++ appdemo/src/main/java/com/dan/admin/AdminPageController.java
@@ -2,6 +2,7 @@ package com.dan.admin;
 
 import javax.ws.rs.GET;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +11,7 @@ public class AdminPageController {
 	
 	@GET
 	@RequestMapping(value ="/admin")
+	@Secured(value = {"ROLE_ADMIN"})
 	
 	public String openAdminMainPage() {
 		
